7c107031c1c1916ff16ef967c3b39b11a207a08c
JotaZen <106561804+JotaZen@users.noreply.github.com>
2022-07-12 10:21:56 -0400

    Update habitaciones.py

--- habitaciones.py
+++ habitaciones.py
@@ -1,31 +1,31 @@
 Class HabitacionDoble:
-  tipo = "Habitacion Doble"
-  descripcion = "Habitacion para dos personas con baño privado y Desayuno incluido"
-  ocupacion_max = 2
-  tarifa_online = 77900
-  comodidades = ["Televisión por cable" , "Secador de Cabello"]
-  estado = "libre"
-  nombre = ""
+    tipo = "Habitacion Doble"
+    descripcion = "Habitacion para dos personas con baño privado y Desayuno incluido"
+    ocupacion_max = 2
+    tarifa_online = 77900
+    comodidades = ["Televisión por cable" , "Secador de Cabello"]
+    estado = "libre"
+    nombre = ""
   
 Class HabitacionQuintuple:
-  tipo = "Habitacion Quintuple"
-  descripcion = "Habitacion para 5 personas con una cama matrimonial y 3 camas individuales. Baño privado."
-  ocupacion_max = 5
-  tarifa_online = 109250
-  comodidades = ["Televisión por cable" , "Secador de Cabello", "WiFi"]
-  estado = "libre"
-  nombre = ""
+    tipo = "Habitacion Quintuple"
+    descripcion = "Habitacion para 5 personas con una cama matrimonial y 3 camas individuales. Baño privado."
+    ocupacion_max = 5
+    tarifa_online = 109250
+    comodidades = ["Televisión por cable" , "Secador de Cabello", "WiFi"]
+    estado = "libre"
+    nombre = ""
   
 Class DobleEconomy:
-  tipo = "DobleEconomy"
-  descripcion = "Habitación para dos personas con cama doble, baño privado y desayuno incluido"
-  ocupacion_max = 2
-  tarifa_online = 65550
-  comodidades = ["Televisión por cable" , "Secador de Cabello", "WiFi"]
-  estado = "libre"
-  nombre = ""
+    tipo = "DobleEconomy"
+    descripcion = "Habitación para dos personas con cama doble, baño privado y desayuno incluido"
+    ocupacion_max = 2
+    tarifa_online = 65550
+    comodidades = ["Televisión por cable" , "Secador de Cabello", "WiFi"]
+    estado = "libre"
+    nombre = ""
   
-  plancoet = DobleEconomy(nombre = "Plancoet")
-  print(plancoet.nombre)
-  paimpol = DobleEconomy(nombre = "Paimpol")
-  print(paimpol.nombre)
+ plancoet = DobleEconomy(nombre = "Plancoet")
+ print(plancoet.nombre)
+ paimpol = DobleEconomy(nombre = "Paimpol")
+ print(paimpol.nombre)
