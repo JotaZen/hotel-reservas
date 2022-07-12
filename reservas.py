2b412fdf3583fa4abde76c689dc230b0cdf4da22
JotaZen <106561804+JotaZen@users.noreply.github.com>
2022-07-12 11:08:48 -0400

    Update reservas.py

--- reservas.py
+++ reservas.py
@@ -1,1 +1,8 @@
 
+from habitaciones import HabitacionDoble, HabitacionQuintuple, DobleEconomy
+from datetime import date, datetime
+
+def tiempoEstadia(entrada, salida):
+    #date
+    x = salida - entrada   
+    return x.days
