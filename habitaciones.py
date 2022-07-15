ead3cf191c5908e0a51f0a118ed9cd31922ed5c1
JotaZen <106561804+JotaZen@users.noreply.github.com>
2022-07-15 11:51:43 -0400

    Update habitaciones.py

--- habitaciones.py
+++ habitaciones.py
@@ -1,38 +1,44 @@
-#Clase habitaciones!#
-class HabitacionDoble:
+class Habitacion:
+
+    #--Atributos--#
+    nombre = str
+    tipo = "Habitacion"
+    descripcion = "Habitacion"
+    ocupacion_max = 0
+    tarifa_online = 0
+    comodidades = ["Televisión por cable" , "Secador de Cabello", "WiFi"]
+    estado = "libre"
+    habitaciones = []
+    diasReservado = []
+ 
+    #--Acciones--#
+    def __init__(habitacion, nombre):
+        habitacion.nombre = nombre
+
+    def __str__(habitacion):
+        return habitacion.nombre
+
+class HabitacionDoble(Habitacion):
     tipo = "Habitacion Doble"
     descripcion = "Habitacion para dos personas con baño privado y Desayuno incluido"
     ocupacion_max = 2
     tarifa_online = 77900
-    comodidades = ["Televisión por cable" , "Secador de Cabello"]
-    estado = "libre"
-    nombre = ""
-    habitaciones = []
-    def __init__(self, nombre):
-        self.nombre = nombre
-    
-class HabitacionQuintuple:
+
+class HabitacionQuintuple(Habitacion):
     tipo = "Habitacion Quintuple"
     descripcion = "Habitacion para 5 personas con una cama matrimonial y 3 camas individuales. Baño privado."
     ocupacion_max = 5
     tarifa_online = 109250
-    comodidades = ["Televisión por cable" , "Secador de Cabello", "WiFi"]
-    estado = "libre"
-    nombre = ""
-    habitaciones = []
-    def __init__(self, nombre):
-        self.nombre = nombre
   
-class DobleEconomy:
+class DobleEconomy(Habitacion):
     tipo = "DobleEconomy"
     descripcion = "Habitación para dos personas con cama doble, baño privado y desayuno incluido"
     ocupacion_max = 2
     tarifa_online = 65550
-    comodidades = ["Televisión por cable" , "Secador de Cabello", "WiFi"]
-    estado = "libre"
-    nombre = "asd"
-    habitaciones = []
-    def __init__(self, nombre):
-        self.nombre = nombre
-  
+
+class Cliente:
+    nombres = ""
+    apellidos = ""
+    teléfono = ""
+    
   
