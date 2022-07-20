c46729daacc88a6512675c3b523960fd5d980ef7
JotaZen <106561804+JotaZen@users.noreply.github.com>
2022-07-19 23:46:40 -0400

    Update habitaciones.py

--- habitaciones.py
+++ habitaciones.py
@@ -1,23 +1,26 @@
 class Habitacion:
 
     #--Atributos--#
-    nombre = str
     tipo = "Habitacion"
     descripcion = "Habitacion"
     ocupacion_max = 0
     tarifa_online = 0
     comodidades = ["Televisión por cable" , "Secador de Cabello", "WiFi"]
     estado = "libre"
+    habitaciones = []
     todas_habitaciones = []
-    dias_reservados = []
  
     #--Acciones--#
     def __init__(habitacion, nombre):
         habitacion.nombre = nombre
+        habitacion.dias_reservados = []
         habitacion.todas_habitaciones.append([habitacion.tipo,(nombre)])
 
     def __str__(habitacion):
         return habitacion.nombre
+
+    def reservar(habitacion, dia):
+        habitacion.dias_reservados.append(dia)
 
 class HabitacionDoble(Habitacion):
     tipo = "Habitacion Doble"
@@ -37,11 +40,6 @@ class DobleEconomy(Habitacion):
     ocupacion_max = 2
     tarifa_online = 65550
 
-class Cliente:
-    nombres = ""
-    apellidos = ""
-    teléfono = ""
-    
   
 
   
